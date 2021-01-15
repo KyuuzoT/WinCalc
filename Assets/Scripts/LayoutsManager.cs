af28c793ea6f5ffa614848b0e5d72c9e47b3638c
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2021-01-15 16:25:40 +0300

    Temporal suspension 15.01.2021 16.25

--- Assets/Scripts/LayoutsManager.cs
+++ Assets/Scripts/LayoutsManager.cs
@@ -93,22 +93,22 @@ public class LayoutsManager : LayoutsManagerBase
                 AddSymbolToExpression("0");
                 break;
             case "DivideButton":
-                AddSymbolToExpression("0");
+                AddOperationToExpression("/");
                 break;
             case "MultiplyButton":
-                AddSymbolToExpression("0");
+                AddOperationToExpression("/");
                 break;
             case "MinusButton":
-                AddSymbolToExpression("0");
+                AddOperationToExpression("/");
                 break;
             case "PlusButton":
-                AddSymbolToExpression("0");
+                AddOperationToExpression("/");
                 break;
             case "EqualsButton":
                 AddSymbolToExpression("0");
                 break;
             case "PlusMinusButton":
-                AddSymbolToExpression("0");
+                AddSymbolToExpression("-");
                 break;
             case "Expression":
                 AddSymbolToExpression("0");
@@ -121,46 +121,111 @@ public class LayoutsManager : LayoutsManagerBase
         char operation = Regex.Match(Expression, RegexPattern).Value.FirstOrDefault();
         string[] splittedExpression = Expression.Split(operation);
 
-        splittedExpression[1] = "0";
-        Expression = $"{splittedExpression[0]}{operation}{splittedExpression[1]}";
-    }
-
-    private string firstPart;
-    private string secondPart;
-
-    private void AddSymbolToExpression(string symbol)
-    {
-        int number;
-        int lastSymbol;
-
-        //If last symbol of expression is digit
-        if (int.TryParse(Expression.Last().ToString(), out lastSymbol))
+        if(splittedExpression.Length == 1)
         {
-            //If symbol is digit
-            if (int.TryParse(symbol, out number))
-            {
-                if (lastSymbol.Equals(0))
-                {
-                    Expression = Expression.Replace(Expression.Last().ToString(), number.ToString());
-                }
-                else
-                {
-                    Expression = $"{Expression}{number}";
-                }
-            }
-            else
-            {
-
-            }
+            splittedExpression[0] = "0";
+            Expression = splittedExpression[0];
+        }
+        else
+        {
+            splittedExpression[1] = "0";
+            Expression = $"{splittedExpression[0]}{operation}{splittedExpression[1]}";
         }
     }
 
-    private void ButtonClicked(int index)
+    private void AddSymbolToExpression(string input)
     {
-        //switch (index)
-        //{
-        //    default:
-        //        break;
-        //}
+        string firstPart = string.Empty;
+        string secondPart = string.Empty;
+        char operation = Regex.Match(Expression, RegexPattern).Value.FirstOrDefault();
+        string[] splittedExpression = Expression.Split(operation);
+
+        firstPart = splittedExpression[0];
+        if(splittedExpression.Length > 1)
+        {
+            secondPart = splittedExpression[1];
+        }
+
+        if(input.Contains('.'))
+        {
+            firstPart = DotValidation(firstPart, input);
+            if(!string.IsNullOrEmpty(secondPart))
+            {
+                secondPart = DotValidation(secondPart, input);
+            }
+        }
+        else if(input.Contains('0'))
+        {
+            firstPart = ZeroValidation(firstPart, input);
+            if (!string.IsNullOrEmpty(secondPart))
+            {
+                secondPart = DotValidation(secondPart, input);
+            }
+        }
+        else if(input.Contains('-'))
+        {
+            firstPart = PlusMinusValidation(firstPart, input);
+        }
+
+        Expression = $"{firstPart}{operation}{secondPart}";
+    }
+
+    private string PlusMinusValidation(string valStr, string input)
+    {
+        if(input.Contains('-'))
+        {
+            return $"{input}{valStr}";
+        }
+        return valStr;
+    }
+
+    private string ZeroValidation(string valStr, string input)
+    {
+        if(input.Contains('0'))
+        {
+            if(valStr.Contains('.'))
+            {
+                return $"{valStr}{0}";
+            }
+            else
+            {
+                if(valStr.Length == 1 && valStr.First().Equals('0'))
+                {
+                    return valStr;
+                }
+
+                if(valStr.Length > 1)
+                {
+                    return $"{valStr}{input}";
+                }
+            }
+        }
+        throw new NotImplementedException();
+    }
+
+    private string DotValidation(string valStr, string input)
+    {
+        if (input.First().Equals('.') && valStr.Contains('.'))
+        {
+            return valStr;
+        }
+        else if (input.First().Equals('.') && !valStr.Contains('.'))
+        {
+            return $"{valStr}{input}";
+        }
+
+        return valStr;
+    }
+
+    private void AddOperationToExpression(string input)
+    {
+        if(!Regex.IsMatch(Expression, RegexPattern))
+        {
+            Expression = $"{Expression}{input}";
+        }
+        else
+        {
+            return;
+        }
     }
 }
